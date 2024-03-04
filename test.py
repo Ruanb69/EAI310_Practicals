d4e55f55c826abc9354db9443bdd8c47defb9d88
Ruan Bisschoff <u21481700@tuks.co.za>
2024-03-04 19:39:03 +0200

    Added comments

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 
 class Node:
-
-
+    #class for nodes in the search tree
     def __init__(self, move):
         self.Move = move
         self.R = None
@@ -16,11 +15,14 @@ class Node:
 
 
 def insertNode(root, newMove):
+#function to insert a new node into the search tree
+    #check if there is a root node
     if root == None:
         root = Node(newMove)
         root.parent = None
         return root
-
+    # try to add a R node if root.R is None
+    # return root if successful
     if root.R is None and newMove == "R":
         if root.height>=5:
             return root
@@ -29,6 +31,8 @@ def insertNode(root, newMove):
         root.R.height=root.height+1
         return root
 
+    # try to add a P node if root.P is None
+    # return root if successful
     if root.P is None and newMove == "P":
         if root.height>=5:
             return root
@@ -37,6 +41,8 @@ def insertNode(root, newMove):
         root.P.height=root.height+1
         return root
 
+    # try to add a S node if root.S is None
+    # return root if successful
     if root.S is None and newMove == "S":
         if root.height>=5:
             return root
@@ -45,6 +51,8 @@ def insertNode(root, newMove):
         root.S.height=root.height+1
         return root
 
+    #recursive call of insertnode for all the moves
+
     insertNode(root.R,newMove)
     insertNode(root.P,newMove)
     insertNode(root.S,newMove)
@@ -52,40 +60,7 @@ def insertNode(root, newMove):
 
 
 
-    # if root.R is None:
-    #     if root.height>5:
-    #         return root
-    #     root.R = Node(newMove)
-    #     root.R.parent = root
-    #     root.R.height=root.height+1
-    # else:
-    #     insertNode(root.R, newMove)
-    # # print(newMove)
-    # return root
-    #
-    # if root.P is  None:
-    #     if root.height>5:
-    #         return root
-    #     root.P = Node(newMove)
-    #     root.P.parent = root
-    #     root.P.height = root.height + 1
-    # else:
-    #     insertNode(root.P, newMove)
-    # # print(newMove)
-    # return root
-    #
-    # if root.S is  None:
-    #     if root.height>5:
-    #         return root
-    #     root.S = Node(newMove)
-    #     root.S.parent = root
-    #     root.S.height = root.height + 1
-    # else:
-    #     insertNode(root.S, newMove)
-    # # print(newMove)
-    # return root
-    # else:
-    #     print("Unknown move")
+
 ###########################################################################################################################
 
 
@@ -94,6 +69,7 @@ def insertNode(root, newMove):
 
 
 def getPlaySequence(Node):
+# function to aquire the play sequence of a node which consists of parents of a node
     outArray = [Node.Move]
     parent = Node.parent
     while parent.Move is not None:
@@ -102,6 +78,7 @@ def getPlaySequence(Node):
     return outArray
 
 def makeSearchTree(root):
+# function to initialy create the search tree
     for i in range(81):
         insertNode(root, "R")
         insertNode(root, "P")
@@ -110,30 +87,27 @@ def makeSearchTree(root):
 
 
 def iterativeBFS(root):
-    if root is None:
+# BFS search to aquire the next sequence to be played
+    if root is None:        #check if root is None
         print("No root")
         return root
     else:
-        q=[]
-        root.visited=True
+        q=[]        #initialize the queue
+        root.visited = True
         q.append(root)
-    step=0
 
-    while len(q)>0:
-        step+=1
-        currNode=q.pop(0)
 
-        if currNode.Move is not None and not currNode.visited:
+    while len(q)>0: #check if there is a node in the queue
+
+        currNode=q.pop(0)       #get the currNode from the stack
+
+        if currNode.Move is not None and not currNode.visited:  #check if currennt node is valid and not yet visited
             currNode.visited=True
-            # global prevPlaySequence
-            # prevPlaySequence= playSequence
-            # global playSequence
             playSequence= getPlaySequence(currNode)
             return playSequence
 
+        # append currNode's children to the queue
 
-
-        # out = ""
         if currNode.R is not None:
             # currNode.R.visited=True
             parent=currNode.parent
@@ -144,7 +118,7 @@ def iterativeBFS(root):
             # print("step " + str(step) + ": " + out)
             q.append(currNode.R)
 
-        # out = ""
+
         if currNode.P is not None:
             # currNode.P.visited = True
             parent = currNode.parent
@@ -182,20 +156,18 @@ def getCorrectMove(Move):
 
 
 if input == "":
+    #initialize all variables
 
     Counter=0
     print(" round "+ str(Counter))
     Counter +=1
-
-    root=None
-    root = insertNode(root, None)
+    root = insertNode(None, None)
     makeSearchTree(root)
     history = ["X", "X"]
     outputSequence = []
     print("round Start")
     Seq_exec_flag = False
     correctFlag = False
-    # correctSequenceCounter=0
     correctSequence = []
     prevPlaySequence = []
     playSequence = []
@@ -206,27 +178,27 @@ if input == "":
 else:
     print(" round "+ str(Counter) )
     Counter += 1
-    if len(history) > 0:
+    if len(history) > 0:  #check if there is a valid history
         history.pop(0)
         print("input : "+input)
         history.append(input)
     else:
         print(history)
 
-    if history[0] == history[1] and not correctFlag:
+    if history[0] == history[1] and not correctFlag:        #check if previous input results in a correct move
         correctFlag = True
         correctSequence = prevPlaySequence.copy()
         playSequence = prevPlaySequence
         print("correct sequence"+str(correctSequence))
 
 
-    if correctFlag and len(playSequence) > 0:
+    if correctFlag and len(playSequence) > 0:       #play the correct sequence
         output = playSequence.pop(0)
         print("move : "+str(output))
 
-    elif correctFlag and len(playSequence) == 0:
+    elif correctFlag and len(playSequence) == 0: #finnish playing correct sequence
 
-        if history[0] == history[1]:
+        if history[0] == history[1]: #check if sequence that was played was correct
 
             playSequence = correctSequence.copy()
             print("counter move :"+ getCorrectMove(history[1]))
@@ -236,17 +208,17 @@ else:
             print("incorrect sequence")
             correctFlag = False
 
-    elif Seq_exec_flag == False or len(outputSequence) == 0:
+    elif Seq_exec_flag == False or len(outputSequence) == 0: # get the and start playing the next sequence in the search tree
         outputSequence = iterativeBFS(root).copy()
         print("sequence"+str(outputSequence))
         prevPlaySequence = outputSequence.copy()
-        Seq_exec_flag = True
+        Seq_exec_flag = True   # flag to indicate that the sequence is being played
         output=outputSequence.pop(0)
     else:
-        if len(outputSequence) > 1:
+        if len(outputSequence) > 1:  # pla the sequence
             output = outputSequence.pop(0)
             print(outputSequence)
-        elif len(outputSequence) == 1:
+        elif len(outputSequence) == 1:  # disalble the flag and get the last move
             print(outputSequence)
             Seq_exec_flag = False
             output = outputSequence.pop(0)
